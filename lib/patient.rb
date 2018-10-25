088bcb0d4690e5effb09ccde61a4484307c62309
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-25 22:46:17 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,20 +1,26 @@
-class Patient 
-  @@all = []
+class Patient
   
-  attr_reader :name, :appointment, :doctor
+   attr_reader :name, :appointment, :doctor
   
   def initialize(name)
     @name = name
-    @@all << self 
-  end
-
+    @@all = [] 
+    end
   def new_appointment(doctor, date)
-    new_appointment = Appointment.new(self, )
-  end 
+    Appointment.new(self, doctor, date)
+  end
   
-  #class Method 
+   def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
   
-  def self.all 
-    @@all 
-  end 
-end 
+  
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
+  end
+  
+end
