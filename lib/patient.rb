9428714943e2534c6ceafe9ce7cb903e85aed471
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-25 22:47:15 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,21 +6,27 @@ attr_reader :name, :appointment, :doctor
     @name = name
     @@all << self  
   end
+  
   def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
   
-   def appointments
+  def appointments
     Appointment.all.select do |appointment|
       appointment.patient == self
     end
   end
-  
   
   def doctors
     Appointment.all.map do |appointment|
       appointment.doctor
     end
   end
+  
+  #class Method 
+  
+  def self.all 
+    @@all 
+  end 
   
 end
